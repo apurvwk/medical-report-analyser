438835ad48a42bb6b9aa553379bd6651277f8f4d
apurvgk <apurvgk@gmail.com>
2025-06-22 13:24:59 +0530

    Mapping public IP at the launch

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,9 +23,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "pdf-processor-subnet"
